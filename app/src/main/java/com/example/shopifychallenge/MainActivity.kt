83a4d68cb30e6600c1cc352bec0acdbc55d0d9e1
Zito Namuro <potto1000@gmail.com>
2019-09-20 21:21:53 -0400

    Added random decks, scalable deck sized and pair matching features. Basic features in functional state

--- app/src/main/java/com/example/shopifychallenge/MainActivity.kt
+++ app/src/main/java/com/example/shopifychallenge/MainActivity.kt
@@ -14,24 +14,26 @@ import org.jetbrains.anko.uiThread
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var productsAdapter: ProductsAdapter
+    lateinit var gameProductsAdapter: ProductsAdapter
+
+    val productArray = ArrayList<Product>()
+    val gameProductsArray = ArrayList<Product>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val productArray = ArrayList<Product>()
-
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = GridLayoutManager(this, 5)
-        productsAdapter = ProductsAdapter(productArray)
-        recyclerView.adapter = productsAdapter
+        gameProductsAdapter = ProductsAdapter(gameProductsArray, recyclerView)
+        recyclerView.adapter = gameProductsAdapter
 
         doAsync {
-
             var jsonString = URL("https://shopicruit.myshopify.com/admin/products.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6").readText()
             val productsObject = JSONObject(jsonString)
             val productsArray = productsObject.getJSONArray("products")
@@ -40,12 +42,33 @@ class MainActivity : AppCompatActivity() {
                 val imagesArray = productObject.getJSONArray("images")
                 val imageSrc = imagesArray.getJSONObject(0).getString("src")
                 val bitmap = BitmapFactory.decodeStream(URL(imageSrc).openConnection().getInputStream())
-                Log.i("Test", imageSrc)
                 productArray.add(Product(productObject.getInt("id"), productObject.getString("title"), bitmap))
             }
             uiThread {
-                productsAdapter.notifyDataSetChanged()
+                Shuffle(10)
+                gameProductsAdapter.notifyDataSetChanged()
             }
         }
+    }
+
+    fun Shuffle(numberOfCards : Int) {
+
+        val randomProducts = ArrayList<Int>()
+
+        gameProductsArray.clear()
+        productArray.shuffle()
+
+        for (i in 0 until productArray.size) {
+            randomProducts.add(i)
+        }
+
+        randomProducts.shuffle()
+
+        for (i in 0 until numberOfCards/2) {
+            gameProductsArray.add(productArray[i])
+            gameProductsArray.add(productArray[i])
+        }
+
+        gameProductsArray.shuffle()
     }
 }
--- app/src/main/java/com/example/shopifychallenge/ProductsAdapter.kt
+++ app/src/main/java/com/example/shopifychallenge/ProductsAdapter.kt
@@ -4,6 +4,9 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Handler
+import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +15,10 @@ import android.view.animation.DecelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.card_layout.view.*
 
-class ProductsAdapter(val products: ArrayList<Product>) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: RecyclerView) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+
+    var pick1 = -1
+    var pick2 = -1
 
     /**
      * Return the view for each item in the list
@@ -29,18 +35,25 @@ class ProductsAdapter(val products: ArrayList<Product>) : RecyclerView.Adapter<P
         holder.bindItems(products[holder.adapterPosition])
 
         holder.itemView.setOnClickListener {
-            val oa1 = ObjectAnimator.ofFloat(holder.itemView, "scaleX", 1f, 0f)
-            val oa2 = ObjectAnimator.ofFloat(holder.itemView, "scaleX", 0f, 1f)
-            oa1.interpolator = DecelerateInterpolator()
-            oa2.interpolator = AccelerateDecelerateInterpolator()
-            oa1.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    //imageView.setImageResource(android.R.drawable.frontSide)
-                    oa2.start()
+            if (holder.itemView.tag == "back") {
+                flipCardToFront(holder.itemView)
+                if (pick1 == -1 && pick2 == -1) {
+                    pick1 = holder.adapterPosition
+                } else if (pick2 == -1) {
+                    pick2 = holder.adapterPosition
                 }
-            })
-            oa1.start()
+            }
+
+            if (pick1 != -1 && pick2 != -1) {
+                if (products[pick1].id == products[pick2].id) {
+
+                } else {
+                    flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick1)!!.itemView)
+                    flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick2)!!.itemView)
+                }
+                pick1 = -1
+                pick2 = -1
+            }
         }
     }
 
@@ -51,6 +64,47 @@ class ProductsAdapter(val products: ArrayList<Product>) : RecyclerView.Adapter<P
         return products.size
     }
 
+    fun flipCardToFront(view: View) {
+        val oa1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f)
+        val oa2 = ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f)
+        oa1.duration = 200
+        oa2.duration = 200
+        oa1.interpolator = DecelerateInterpolator()
+        oa2.interpolator = AccelerateDecelerateInterpolator()
+        oa1.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                view.tag = "front"
+                view.title.visibility = View.VISIBLE
+                view.image.visibility = View.VISIBLE
+                view.backImage.visibility = View.GONE
+                oa2.start()
+            }
+        })
+        oa1.start()
+    }
+
+    fun flipCardToBack(view: View) {
+        val oa1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f)
+        val oa2 = ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f)
+        oa1.duration = 200
+        oa2.duration = 200
+        oa1.interpolator = DecelerateInterpolator()
+        oa2.interpolator = AccelerateDecelerateInterpolator()
+        oa1.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                view.tag = "back"
+                view.backImage.visibility = View.VISIBLE
+                view.title.visibility = View.GONE
+                view.image.visibility = View.GONE
+                oa2.start()
+            }
+        })
+        oa1.startDelay = 1000
+        oa1.start()
+    }
+
     /**
      * Holds the custom list view
      */
@@ -59,6 +113,8 @@ class ProductsAdapter(val products: ArrayList<Product>) : RecyclerView.Adapter<P
         fun bindItems(product: Product) {
             itemView.title.text=product.title
             itemView.image.setImageBitmap(product.imgBitmap)
+            itemView.tag = "back"
         }
     }
+
 }
