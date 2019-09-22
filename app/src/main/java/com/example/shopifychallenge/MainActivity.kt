126032a0b7846f308a8d80ddd17162679251bd06
Zito Namuro <potto1000@gmail.com>
2019-09-22 16:11:13 -0400

    Added user input, grid resizing feature and matches configuration feature

--- app/src/main/java/com/example/shopifychallenge/MainActivity.kt
+++ app/src/main/java/com/example/shopifychallenge/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.shopifychallenge
 
+import android.content.DialogInterface
 import android.graphics.BitmapFactory
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
@@ -21,14 +26,19 @@ class MainActivity : AppCompatActivity() {
     val productArray = ArrayList<Product>()
     val gameProductsArray = ArrayList<Product>()
 
+    var cards = 20
+    var matches = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = GridLayoutManager(this, 5)
+        recyclerView.layoutManager = GridLayoutManager(this, 8)
         gameProductsAdapter = ProductsAdapter(gameProductsArray, recyclerView)
         recyclerView.adapter = gameProductsAdapter
+
+        val loadingPanel = findViewById<RelativeLayout>(R.id.loadingPanel)
 
         setSupportActionBar(findViewById(R.id.toolbar))
 
@@ -44,7 +54,8 @@ class MainActivity : AppCompatActivity() {
                 productArray.add(Product(productObject.getInt("id"), productObject.getString("title"), bitmap))
             }
             uiThread {
-                Shuffle(20)
+                loadingPanel.visibility = View.GONE
+                Shuffle(cards)
             }
         }
     }
@@ -57,11 +68,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_settings -> {
             // User chose the "Print" item
-            Toast.makeText(this,"Settings action",Toast.LENGTH_LONG).show()
+            ShowSettingsDialog()
             true
         }
         R.id.action_shuffle ->{
-            Shuffle(10)
+            Shuffle(cards)
             true
         }
 
@@ -87,12 +98,99 @@ class MainActivity : AppCompatActivity() {
 
         randomProducts.shuffle()
 
-        for (i in 0 until numberOfCards/2) {
-            gameProductsArray.add(Product(productArray[i].id, productArray[i].title, productArray[i].imgBitmap))
-            gameProductsArray.add(Product(productArray[i].id, productArray[i].title, productArray[i].imgBitmap))
+        for (i in 0 until numberOfCards/matches) {
+            for (j in 0 until matches) {
+                gameProductsArray.add(Product(productArray[i].id, productArray[i].title, productArray[i].imgBitmap))
+            }
         }
 
         gameProductsArray.shuffle()
         gameProductsAdapter.notifyDataSetChanged()
+    }
+
+    fun ShowSettingsDialog() {
+        val builder = AlertDialog.Builder(this)
+
+        val customLayout = layoutInflater.inflate(R.layout.settings_dialog, null)
+        builder.setView(customLayout)
+
+        val matchesSeekBar = customLayout.findViewById<SeekBar>(R.id.matchesSeekBar)
+        val matchesText = customLayout.findViewById<TextView>(R.id.matchesText)
+
+        val cardsSeekBar = customLayout.findViewById<SeekBar>(R.id.cardsSeekBar)
+        val cardsText = customLayout.findViewById<TextView>(R.id.cardsText)
+
+        matchesSeekBar.min = 2
+        matchesSeekBar.max = 4
+        matchesSeekBar.progress = matches
+        matchesSeekBar.incrementProgressBy(1)
+
+        matchesText.text = Integer.toString(matches)
+
+        matchesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbar: SeekBar?, i: Int, b: Boolean) {
+                matchesText.text = Integer.toString(i)
+                cardsSeekBar.max = 0
+                cardsSeekBar.max = 50 * matchesSeekBar.progress
+                cardsSeekBar.min = matchesSeekBar.progress
+                cardsSeekBar.progress = matchesSeekBar.progress
+                cardsSeekBar.incrementProgressBy(matchesSeekBar.progress)
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        cardsSeekBar.min = matchesSeekBar.progress
+        cardsSeekBar.max = 50 * matchesSeekBar.progress
+        cardsSeekBar.setProgress(cards)
+        cardsSeekBar.incrementProgressBy(matchesSeekBar.progress)
+
+        cardsText.text = Integer.toString(matchesSeekBar.progress)
+
+        cardsSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbar: SeekBar?, i: Int, b: Boolean) {
+                var progress = i / matchesSeekBar.progress;
+                progress = progress * matchesSeekBar.progress;
+                cardsText.text = Integer.toString(progress)
+            }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        builder.setTitle("Customize New Game")
+        builder.setPositiveButton("Shuffle!") { _, _ ->
+            cards = cardsSeekBar.progress
+            matches = matchesSeekBar.progress
+            Shuffle(cards)
+        }
+        builder.setNegativeButton("Cancel") { _, _ ->
+
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    fun ShowVictory() {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("You Win!")
+        builder.setPositiveButton("Play Again") { _, _ ->
+            Shuffle(cards)
+        }
+        builder.setNegativeButton("View Board") { _, _ ->
+
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/shopifychallenge/ProductsAdapter.kt
+++ app/src/main/java/com/example/shopifychallenge/ProductsAdapter.kt
@@ -21,6 +21,11 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
 
     var pick1 = -1
     var pick2 = -1
+    var pick3 = -1
+    var pick4 = -1
+    var cardsFlipped = 0
+    var matches  = 0
+    var matchesMade = 0
 
     /**
      * Return the view for each item in the list
@@ -38,23 +43,67 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
 
         holder.itemView.setOnClickListener {
             if (products[holder.adapterPosition].side == "back") {
-                flipCardToFront(holder.itemView, products[holder.adapterPosition])
-                if (pick1 == -1 && pick2 == -1) {
+                matches = (holder.itemView.context as MainActivity).matches
+
+                if (cardsFlipped == 0) {
                     pick1 = holder.adapterPosition
-                } else if (pick2 == -1) {
+                    flipCardToFront(holder.itemView, products[holder.adapterPosition])
+                } else if (cardsFlipped == 1) {
                     pick2 = holder.adapterPosition
-                }
-            }
+                    flipCardToFront(holder.itemView, products[holder.adapterPosition])
 
-            if (pick1 != -1 && pick2 != -1) {
-                if (products[pick1].id == products[pick2].id) {
+                    if (matches == 2) {
+                        if (products[pick1].id == products[pick2].id) {
+                            cardsFlipped = 0
+                            Snackbar.make(holder.itemView, "Match Made!", Snackbar.LENGTH_SHORT).show()
+                            matchesMade++
+                            checkForWin(holder.itemView)
+                        } else {
+                            flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick1)?.itemView, products[pick1])
+                            flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick2)?.itemView, products[pick2])
+                        }
+                        pick1 = -1
+                        pick2 = -1
+                    }
+                } else if (cardsFlipped == 2 && matches >= 3) {
+                    pick3 = holder.adapterPosition
+                    flipCardToFront(holder.itemView, products[holder.adapterPosition])
 
-                } else {
-                    flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick1)?.itemView, products[pick1])
-                    flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick2)?.itemView, products[pick2])
+                    if (matches == 3) {
+                        if (products[pick1].id == products[pick2].id && products[pick2].id == products[pick3].id) {
+                            cardsFlipped = 0
+                            Snackbar.make(holder.itemView, "Match Made!", Snackbar.LENGTH_SHORT).show()
+                            matchesMade++
+                            checkForWin(holder.itemView)
+                        } else {
+                            flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick1)?.itemView, products[pick1])
+                            flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick2)?.itemView, products[pick2])
+                            flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick3)?.itemView, products[pick3])
+                        }
+                        pick1 = -1
+                        pick2 = -1
+                        pick3 = -1
+                    }
+                } else if (cardsFlipped == 3 && matches == 4) {
+                    pick4 = holder.adapterPosition
+                    flipCardToFront(holder.itemView, products[holder.adapterPosition])
+
+                    if (products[pick1].id == products[pick2].id && products[pick2].id == products[pick3].id && products[pick3].id == products[pick4].id) {
+                        cardsFlipped = 0
+                        Snackbar.make(holder.itemView, "Match Made!", Snackbar.LENGTH_SHORT).show()
+                        matchesMade++
+                        checkForWin(holder.itemView)
+                    } else {
+                        flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick1)?.itemView, products[pick1])
+                        flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick2)?.itemView, products[pick2])
+                        flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick3)?.itemView, products[pick3])
+                        flipCardToBack(recyclerView.findViewHolderForAdapterPosition(pick4)?.itemView, products[pick4])
+                    }
+                    pick1 = -1
+                    pick2 = -1
+                    pick3 = -1
+                    pick4 = -1
                 }
-                pick1 = -1
-                pick2 = -1
             }
         }
     }
@@ -62,6 +111,10 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
     fun reset() {
         pick1 = -1
         pick2 = -1
+        pick3 = -1
+        pick4 = -1
+        cardsFlipped = 0
+        matchesMade = 0
     }
 
     /**
@@ -72,7 +125,6 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
     }
 
     fun flipCardToFront(view: View?, product: Product) {
-        product.side = "front"
         if (view != null) {
             val oa1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f)
             val oa2 = ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f)
@@ -91,10 +143,11 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
             })
             oa1.start()
         }
+        product.side = "front"
+        cardsFlipped++
     }
 
     fun flipCardToBack(view: View?, product: Product) {
-        product.side = "back"
         if (view != null) {
             val oa1 = ObjectAnimator.ofFloat(view, "scaleX", 1f, 0f)
             val oa2 = ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f)
@@ -112,8 +165,24 @@ class ProductsAdapter(val products: ArrayList<Product>, val recyclerView: Recycl
                     oa2.start()
                 }
             })
+            oa2.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    cardsFlipped--
+                }
+            })
             oa1.startDelay = 1000
             oa1.start()
+        }
+        else {
+            cardsFlipped--
+        }
+        product.side = "back"
+    }
+
+    fun checkForWin(itemView : View) {
+        if (matchesMade == (itemView.context as MainActivity).cards/2) {
+            (itemView.context as MainActivity).ShowVictory()
         }
     }
 
